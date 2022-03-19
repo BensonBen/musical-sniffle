0e0eaa62d6fda28c0d06d3d8c31d81c6c4717097
Benjamin Benson <bensonb@mail.gvsu.edu>
2022-03-19 14:46:09 -0400

    initial commit

---
+++ apps/canny-edge-detection-e2e/src/integration/app.spec.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('canny-edge-detection', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome canny-edge-detection');
+  });
+});
